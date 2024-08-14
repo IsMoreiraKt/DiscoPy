b2e3d9f9d8b0f3ddf6c4ee61c6c1f534b2884977
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-08-14 04:22:10 +0100

    feat: Added definition of the command called play for the Discord bot that allows the user to play a song in the voice channel they are in.

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -16,3 +16,20 @@ ytdl_opts = {
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    
+    @commands.command()
+    async def play(self, ctx, *, query: str):
+        if ctx.author.voice is None:
+            await ctx.send("You need to be in a voice channel to use this command.")
+            return
+
+        channel = ctx.author.voice.channel
+        voice_client = await channel.connect()
+
+        if 'http' in query:
+            url = query
+            await self.play_url(ctx, voice_client, url)
+        else:
+            url = search_youtube(query)
+            await self.play_url(ctx, voice_client, url)
