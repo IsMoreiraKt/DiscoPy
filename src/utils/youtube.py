9b1cd1de7c6fa25d1ef3e248ac709d4ae2018b99
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-08-14 04:38:38 +0100

    feat: Added method that gets and returns a list of audio URLs for all videos in a YouTube playlist.

--- src/utils/youtube.py
+++ src/utils/youtube.py
@@ -5,5 +5,13 @@ import youtube_dl
 def search_youtube(query):
     ytdl = youtube_dl.YoutubeDL({'format': 'bestaudio/best', 'noplaylist': True})
     info = ytdl.extract_info(f"ytsearch:{query}", download=False)
-    
+
     return info['entries'][0]['url']
+
+
+
+def get_playlist(url):
+    ytdl = youtube_dl.YoutubeDL({'format': 'bestaudio/best'})
+    info = ytdl.extract_info(url, download=False)
+
+    return [entry['url'] for entry in info['entries']]
