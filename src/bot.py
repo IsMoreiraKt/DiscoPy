a080fdf15fc30e0f79c7750bd1c0f08c1efa0b84
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-08-14 04:05:35 +0100

    feat: Added bot permissions settings.

--- src/bot.py
+++ src/bot.py
@@ -7,3 +7,8 @@ import os
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.presences = True
